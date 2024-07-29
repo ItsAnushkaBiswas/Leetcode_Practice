6bcb80d17c7ff2a5d751998070deebd807d2526e
Anushka Biswas <anushkabiswas796@gmail.com>
2024-07-29 19:07:23 +0530

    Time: 23 ms (92.14%) | Memory: 46.5 MB (42.50%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -7,8 +7,9 @@ class Solution {
         int result = 0;
         for(int i=0;i<nums.length;i++){
             sum += nums[i];
-            if(arry_sums.containsKey(sum-k))
+            if(arry_sums.containsKey(sum-k)){
                 result += arry_sums.get(sum-k);
+            }
             arry_sums.put(sum, arry_sums.getOrDefault(sum,0)+1);
         }
         return result;
