7d694c424bc0125221398350b904350a5fb8174d
Anushka Biswas <anushkabiswas796@gmail.com>
2024-08-03 15:33:55 +0530

    Time: 4 ms (54.32%) | Memory: 44.2 MB (42.83%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,9 +1,14 @@
 class Solution {
-  public boolean canBeEqual(int[] target, int[] arr) {
-    return Arrays.stream(arr)
-        .boxed()
-        .collect(Collectors.groupingBy(Integer::intValue, Collectors.counting()))
-        .equals(Arrays.stream(target).boxed().collect(
-            Collectors.groupingBy(Integer::intValue, Collectors.counting())));
-  }
+
+    public boolean canBeEqual(int[] target, int[] arr) {
+        // Sort the target array in-place
+        Arrays.sort(target);
+        // Sort the arr array in-place
+        Arrays.sort(arr);
+      
+        // Check if the sorted arrays are equal
+        // The equals method checks if the two arrays have the same elements in the same order
+        return Arrays.equals(target, arr);
+    }
+ 
 }
