f4ad33025f7fc79f74e60867c3e0422a914eb6ca
Anushka Biswas <anushkabiswas796@gmail.com>
2024-08-05 15:19:44 +0530

    Time: 6 ms (91.98%) | Memory: 44.1 MB (51.23%) - LeetSync

--- 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.java
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.java
@@ -1,14 +1,23 @@
 class Solution {
-  public String kthDistinct(String[] arr, int k) {
-    Map<String, Integer> count = new HashMap<>();
+    public String kthDistinct(String[] arr, int k) {
 
-    for (final String a : arr)
-      count.merge(a, 1, Integer::sum);
-
-    for (final String a : arr)
-      if (count.get(a) == 1 && --k == 0)
-        return a;
-
-    return "";
-  }
+        Map<String, Integer> frequencyMap = new HashMap<>();
+      
+        for (String element : arr) {
+            frequencyMap.put(element, frequencyMap.getOrDefault(element, 0) + 1);
+        }
+      
+        for (String element : arr) {
+            if (frequencyMap.get(element) == 1) {
+                k--; 
+              
+                
+                if (k == 0) {
+                    return element;
+                }
+            }
+        }
+      
+        return "";
+    }
 }
