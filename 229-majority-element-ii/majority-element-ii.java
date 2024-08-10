215d2c7a8b96886f8d3cc62f67c34d1589d22775
Anushka Biswas <anushkabiswas796@gmail.com>
2024-08-10 16:53:25 +0530

    Time: 2 ms (99.34%) | Memory: 48.3 MB (18.26%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,34 +1,36 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> result = new ArrayList<>();
-        int ele1 = Integer.MIN_VALUE, ele2 = Integer.MIN_VALUE;
-        int c1 = 0, c2 = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(c1 == 0 && nums[i] != ele2) {
-                ele1 = nums[i];
-                c1 = 1;
-            } else if (c2 == 0 && nums[i] != ele1) {
-                ele2 = nums[i];
-                c2 = 1;
-            } else if (ele1 == nums[i]) {
-                c1++;
-            } else if (ele2 == nums[i]) {
-                c2++;
-            } else {
-                c1--;
-                c2--;
+        List<Integer> newls= new ArrayList();
+        int count1=0, count2=0;
+        int el1=Integer.MIN_VALUE;
+        int el2=Integer.MIN_VALUE;
+
+        for(int i=0; i<nums.length; i++){
+            if(count1==0 && el2!=nums[i]){
+                count1=1;
+                el1=nums[i];
             }
-        }
-        // manual check
-        c1 = 0;
-        c2 = 0;
-        int minimum = nums.length/3 + 1;
-        for(int i = 0; i < nums.length; i++) {
-            if(ele1 == nums[i]) c1++;
-            if(ele2 == nums[i]) c2++;
-        }
-        if(c1 >= minimum) result.add(ele1);
-        if(c2 >= minimum) result.add(ele2);
-        return result;
+            else if(count2==0 && el1!=nums[i]){
+                count2=1;
+                el2=nums[i];
+            }
+            else if(nums[i]==el1) count1++;
+            else if(nums[i]==el2) count2++;
+            else{
+                count1--;
+                count2--;
+            }
+        } 
+        // manual check 
+        count1=0;
+        count2=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==el1)count1++;
+            if(nums[i]==el2)count2++;
+        }  
+        int mini=nums.length/3+1;
+        if(count1>=mini)newls.add(el1);
+        if(count2>=mini)newls.add(el2);
+        return newls;
     }
 }
