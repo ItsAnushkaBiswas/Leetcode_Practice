dad9f250bb32ff8bb9976c7d0643d803d8b5170a
Anushka Biswas <anushkabiswas796@gmail.com>
2024-08-10 20:44:27 +0530

    Time: 17 ms (74.29%) | Memory: 43.9 MB (50.92%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -8,24 +8,24 @@ class Solution {
             for(int j=i+1; j<nums.length-2; j++) {
                 if(j>i+1 && nums[j] == nums[j-1])
                     continue;
-                int left = j+1, right = nums.length-1;
-                while(left<right) {
-                    long sum = (long)nums[i]+nums[j]+nums[left]+nums[right];
+                int k = j+1, l = nums.length-1;
+                while(k<l) {
+                    long sum = (long)nums[i]+nums[j]+nums[k]+nums[l];
                     if(sum == target) {
-                        List<Integer> temp = new ArrayList<>();
-                        temp.add(nums[i]);
-                        temp.add(nums[j]);
-                        temp.add(nums[left]);
-                        temp.add(nums[right]);
-                        result.add(temp);
-                        left++;
-                        while(left<right && nums[left] == nums[left-1]) {
-                            left++;
+                        List<Integer> ans = new ArrayList<>();
+                        ans.add(nums[i]);
+                        ans.add(nums[j]);
+                        ans.add(nums[k]);
+                        ans.add(nums[l]);
+                        result.add(ans);
+                        k++;
+                        while(k<l && nums[k] == nums[k-1]) {
+                            k++;
                         }
                     } else if(sum < target) {
-                        left++;
+                        k++;
                     } else {
-                        right--;
+                        l--;
                     }
                 }
             }
